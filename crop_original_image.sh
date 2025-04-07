287954f0032eff71ee1f96e2e49c9df442789fc2
rodolfolotte <rodolfo.lotte@gmail.com>
2025-04-07 09:42:41 -0300

    update crop scripts

--- crop_original_image.sh
+++ crop_original_image.sh
@@ -1,10 +1,11 @@
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <input_folder> <output_folder>"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <input_folder> <output_folder> <size>"
   exit 1
 fi
 
 INPUT_FOLDER="$1"
 OUTPUT_FOLDER="$2"
+SIZE="$3"
 
 # Create output directory if it doesn't exist
 mkdir -p "$OUTPUT_FOLDER"
@@ -18,5 +19,5 @@ for img in "$INPUT_FOLDER"/*.png; do
   base="${filename%.*}"
 
   # Crop into 128x128 tiles
-  convert "$img" -crop 128x128 +repage +adjoin "$OUTPUT_FOLDER/${base}_tile_%03d.png"
+  convert "$img" -crop "$SIZE"x"$SIZE" +repage +adjoin "$OUTPUT_FOLDER/${base}_tile_%03d.png"
 done
